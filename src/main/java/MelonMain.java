7ab654a18ead1cd282d2ff4a7735710d1ef54294
infor.ljh <infor.ljh.com>
2024-05-06 22:33:46 +0900

    3. 앨범재킷 URL 출력 성공 (멘토님 도움으로 가능, 다른 방식으로 시도 하려다 for문 두개로 나눠진걸 합치지 못하여 멘토님 방법으로 하여 출력 성공) 4. 출력만 하는 Main클래스와 크롤링 메서드로 나눠서 작업 시도하였지만 기능별 크롤링 메서드 출력 구문이 합쳐서 출력하는데 실패하여 일단 보류 5. 크롤링 클롤링+매서드, main 출력 클래스 분리 성공

--- src/main/java/MelonMain.java
+++ src/main/java/MelonMain.java
@@ -12,28 +12,8 @@ public class MelonMain {
 
     public static void main(String[] args) throws IOException {
 
-        String URL = "https://www.melon.com/chart/index.htm"; // URL : 크롤링 하고자 하는 사이트 주소를 변수로 만들어 코드 축약
+        MelonCrwaling melonCrwaling = new MelonCrwaling();
 
-        Document melonTopList = Jsoup.connect(URL).get();
-
-        Elements e0 = melonTopList.getElementsByAttributeValue("class", "title");
-        Element chartType = e0.get(1);
-        System.out.println("멜론 차트: " + chartType.text() + "\n");
-
-
-        Elements title = melonTopList.getElementsByAttributeValue("class", "ellipsis rank01"); // TOP100 제목
-        Elements singer = melonTopList.getElementsByAttributeValue("class", "checkEllipsis"); // TOP100 가수
-        Elements albumJacketImageUrl = melonTopList.getElementsByAttributeValue("class","image_typeAll"); // TOP100 앨범자켓 URL -> 링크 파싱 안되는 문제
-
-
-
-        for (int i = 0; i < 100; i++) {
-            Element titleResult = title.get(i);
-            Element singerResult = singer.get(i);
-            String albumJacketImageSrc = albumJacketImageUrl.get(i).child(0).attr("src");
-
-            System.out.println((i + 1) + "위 " + "제목: " + titleResult.text() + " ,가수: " + singerResult.text() + "\n"+" ,앨범재킷 URL: "+albumJacketImageSrc);
-            // TOP100 출력 코드
-        }
+        melonCrwaling.top100();
     }
 }
