e71a8e72fe182d12f106ef02436c4b2ed837e4bb
infor.ljh <infor.ljh.com>
2024-05-06 21:47:38 +0900

    3. 앨범재킷 URL 출력 성공 (멘토님 도움으로 가능, 다른 방식으로 시도 하려다 for문 두개로 나눠진걸 합치지 못하여 멘토님 방법으로 하여 출력 성공) 4. 출력만 하는 Main클래스와 크롤링 메서드로 나눠서 작업 시도하였지만 기능별 크롤링 메서드 출력 구문이 합쳐서 출력하는데 실패하여 일단 보류

--- src/main/java/MelonMain.java
+++ src/main/java/MelonMain.java
@@ -4,6 +4,9 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MelonMain {
 
@@ -13,24 +16,24 @@ public class MelonMain {
 
         Document melonTopList = Jsoup.connect(URL).get();
 
-        Elements e0 = melonTopList.getElementsByAttributeValue("class","title");
+        Elements e0 = melonTopList.getElementsByAttributeValue("class", "title");
         Element chartType = e0.get(1);
-        System.out.println("멜론 차트: "+chartType.text()+"\n");
+        System.out.println("멜론 차트: " + chartType.text() + "\n");
 
 
-        Elements title = melonTopList.getElementsByAttributeValue("class","ellipsis rank01"); // TOP100 제목
-        Elements singer = melonTopList.getElementsByAttributeValue("class","checkEllipsis"); // TOP100 가수
+        Elements title = melonTopList.getElementsByAttributeValue("class", "ellipsis rank01"); // TOP100 제목
+        Elements singer = melonTopList.getElementsByAttributeValue("class", "checkEllipsis"); // TOP100 가수
         Elements albumJacketImageUrl = melonTopList.getElementsByAttributeValue("class","image_typeAll"); // TOP100 앨범자켓 URL -> 링크 파싱 안되는 문제
 
 
-        for (int i = 0; i < 100; i++){
-                Element titleResult = title.get(i);
-                Element singerResult = singer.get(i);
-                Element albumJacketImageUrlResult = albumJacketImageUrl.get(i);
 
-                System.out.println((i+1)+"위 "+"제목: "+titleResult.text()+" ,가수: "+singerResult.text()+"\n"+"앨범재킷 URL: "+albumJacketImageUrlResult.text());
-                // TOP100 출력 코드
-            }
+        for (int i = 0; i < 100; i++) {
+            Element titleResult = title.get(i);
+            Element singerResult = singer.get(i);
+            String albumJacketImageSrc = albumJacketImageUrl.get(i).child(0).attr("src");
 
+            System.out.println((i + 1) + "위 " + "제목: " + titleResult.text() + " ,가수: " + singerResult.text() + "\n"+" ,앨범재킷 URL: "+albumJacketImageSrc);
+            // TOP100 출력 코드
+        }
     }
 }
