169e1cee1e7f34153afb3c11531eb15ce5c20f7a
infor.ljh <infor.ljh.com>
2024-05-07 22:10:06 +0900

    3. 앨범재킷 URL 출력 성공 (멘토님 도움으로 가능, 다른 방식으로 시도 하려다 for문 두개로 나눠진걸 합치지 못하여 멘토님 방법으로 하여 출력 성공) 4. 출력만 하는 Main클래스와 크롤링 메서드로 나눠서 작업 시도하였지만 기능별 크롤링 메서드 출력 구문이 합쳐서 출력하는데 실패하여 일단 보류 5. 목록 조회 일자, 시간대 크롤링 매서드 추가 6. Music 모델링 클래스 추가

---
+++ src/main/java/Music.java
@@ -0,0 +1,58 @@
+public class Music {
+
+    private String rank;
+    private String title;
+    private String singer;
+    private String alum;
+
+    public Music(String rank,String title,String singer,String alum){
+        this.rank = rank;
+        this.title = title;
+        this.singer = singer;
+        this.alum = alum;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSinger() {
+        return singer;
+    }
+
+    public void setSinger(String singer) {
+        this.singer = singer;
+    }
+
+    public String getAlum() {
+        return alum;
+    }
+
+    public void setAlum(String alum) {
+        this.alum = alum;
+    }
+
+    @Override
+    public String toString() {
+        return "Music{" +
+                "rank='" + rank + '\'' +
+                ", title='" + title + '\'' +
+                ", singer='" + singer + '\'' +
+                ", alum='" + alum + '\'' +
+                '}';
+    }
+
+
+}
